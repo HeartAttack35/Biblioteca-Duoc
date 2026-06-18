6b1860588b25a8c52e32a16110fd5cf49a79ee7b
HeartAttack35 <134518346+HeartAttack35@users.noreply.github.com>
2026-06-18 12:42:21 -0400

    Update ReservaModelAssembler.java

--- biblioteca/src/main/java/cl/duoc/biblioteca/assemblers/ReservaModelAssembler.java
+++ biblioteca/src/main/java/cl/duoc/biblioteca/assemblers/ReservaModelAssembler.java
@@ -14,7 +14,9 @@ public class ReservaModelAssembler implements RepresentationModelAssembler<Reser
     @Override
     public EntityModel<Reserva> toModel(Reserva reserva) {
         return EntityModel.of(reserva,
+                // Enlace al recurso individual (self)
                 linkTo(methodOn(ReservaControllerV2.class).getReservaById(reserva.getId())).withSelfRel(),
+                // Enlace a la colección completa (reservas)
                 linkTo(methodOn(ReservaControllerV2.class).getAllReservas()).withRel("reservas"));
     }
 }
